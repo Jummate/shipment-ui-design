39c83f99d912d3bbd847ed3da59fa454b62e91d0
Omololu <yakubjumat@gmail.com>
2024-11-10 17:34:45 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>This is App</div>
+    </>
+  )
+}
+
+export default App
